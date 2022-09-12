c33a937ff35154230bc8ddd5a741c634b062a92e
Donggeon Lee <donggeon.lee@keymedia.com>
2022-09-12 16:08:32 +1000

    327. Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,17 @@
-function SamplePage() {
-    return <h1>the sample page</h1>;
-  }
-  
-  export default SamplePage;
-  
+import { Fragment } from 'react';
+import Link from 'next/link';
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>the news page</h1>
+      <ul>
+        <li>
+          <Link href='/news/abcd'>Go ABCD</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
